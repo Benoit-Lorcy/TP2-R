2a82163f9d1b6edb3cf183feecaf7ebd5205993c
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2022-01-19 17:08:04 +0100

    tableau

--- ex2.r
+++ ex2.r
@@ -1,8 +1,12 @@
 # ex2 TP3
 library(prob)
 
+vecteur1 <- c(1:10) ; vecteur2 <- c(2:11)
+
+
 bourgeonnements = c(0,1,2,3,4,5,6,7,8,9,10)
 contamines = c(482,133,46,24,6,5,2,1,0,1,1)
+tableau <- data.frame("Nombre de bourgeonnements par feuille" = bourgeonnements, "Nombre de feuilles contaminées" = contamines) 
 
 # Pour l’ensemble des questions suivantes, donner les commandes R permettant de
 # répondre aux questions suivantes.
@@ -14,6 +18,8 @@ cat("La taille de l’échantillon est de ",length(bourgeonnements),"\n")
 print("Le carctère spécifique observé est la contamination.\n")
 print("Sa nature est qualitative.")
 # 3. Représenter graphiquement les données ?
-
+# colnames(tableau)
+print(tableau)
+plot(tableau)
 # 4. Calculer la moyenne et l’écart-type empirique du nombre de bourgeonnements
 #    par feuille.

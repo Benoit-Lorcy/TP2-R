bb3539c4cf5b44e23a574a3c6731c0779dfcb519
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2022-01-19 16:31:10 +0100

    ajout des tableaux

--- ex2.r
+++ ex2.r
@@ -1,12 +1,14 @@
 # ex2 TP3
 library(prob)
 
-
+bourgeonnements = c(0,1,2,3,4,5,6,7,8,9,10)
+contamines = c(482,133,46,24,6,5,2,1,0,1,1)
 
 # Pour l’ensemble des questions suivantes, donner les commandes R permettant de
 # répondre aux questions suivantes.
 
 # 1. Quelle est la population statistique observée ? La taille de l’échantillon ?
+
 # 2. Quel est le caractère statistique observé et sa nature ?
 # 3. Représenter graphiquement les données ?
 # 4. Calculer la moyenne et l’écart-type empirique du nombre de bourgeonnements

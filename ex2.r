77187731a3aa683b09b8d8c7f93f384eedbaf2a9
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2022-01-19 16:27:28 +0100

    initialisation

---
+++ ex2.r
@@ -0,0 +1,13 @@
+# ex2 TP3
+library(prob)
+
+
+
+# Pour l’ensemble des questions suivantes, donner les commandes R permettant de
+# répondre aux questions suivantes.
+
+# 1. Quelle est la population statistique observée ? La taille de l’échantillon ?
+# 2. Quel est le caractère statistique observé et sa nature ?
+# 3. Représenter graphiquement les données ?
+# 4. Calculer la moyenne et l’écart-type empirique du nombre de bourgeonnements
+#    par feuille.

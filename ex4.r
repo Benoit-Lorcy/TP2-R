dd170e6f243a556a9adfd3cc9302f10129fde55b
Noam-NS <72453874+Noam-NS@users.noreply.github.com>
2022-01-19 17:58:57 +0100

    Create ex4.r

---
+++ ex4.r
@@ -0,0 +1,2 @@
+//todo: get data
+histogram<-hist(data, breaks=c(0, 20, 50, 100, 200, 250), freq = FALSE, xlab="Superficie (en ha)", ylab="fréquence", main="Histogramme des exploitations (en fréquence)")
